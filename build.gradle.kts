0c61cec4f77ebd4ed2bbd92636b76fd6f7c53038
Andrey Pfau <andreypfau@ton.org>
2024-06-07 13:43:08 +0400

    build with gradle

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("me.champeau.jmh") version "0.7.2"
+}
+
+version = "3.3.2"
+group = "net.fec.openrq"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    jmh("com.beust:jcommander:1.35")
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnit("4.13.2")
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(8)
+    }
+}
